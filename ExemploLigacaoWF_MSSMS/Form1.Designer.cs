8563cd912a062472cc9aaf1aee6061f35b57e59d
Leonardo Monteiro <leonardomonteiro7727@gmail.com>
2024-10-17 21:30:21 +0100

    Adicionar arquivos de projeto.

---
+++ ExemploLigacaoWF_MSSMS/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ExemploLigacaoWF_MSSMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvContatos = new System.Windows.Forms.DataGridView();
+            this.txttelefone = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegistodeUtilizadores = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvContatos
+            // 
+            this.dgvContatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContatos.Location = new System.Drawing.Point(35, 240);
+            this.dgvContatos.Name = "dgvContatos";
+            this.dgvContatos.RowHeadersWidth = 51;
+            this.dgvContatos.RowTemplate.Height = 24;
+            this.dgvContatos.Size = new System.Drawing.Size(896, 378);
+            this.dgvContatos.TabIndex = 22;
+            this.dgvContatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvContatos_CellContentClick);
+            // 
+            // txttelefone
+            // 
+            this.txttelefone.Location = new System.Drawing.Point(738, 98);
+            this.txttelefone.Name = "txttelefone";
+            this.txttelefone.Size = new System.Drawing.Size(193, 22);
+            this.txttelefone.TabIndex = 21;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(388, 98);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(332, 22);
+            this.txtemail.TabIndex = 20;
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(35, 98);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(337, 22);
+            this.txtnome.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(734, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 21);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Telefone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(384, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "eMail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nome";
+            // 
+            // btnRegistodeUtilizadores
+            // 
+            this.btnRegistodeUtilizadores.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnRegistodeUtilizadores.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistodeUtilizadores.Location = new System.Drawing.Point(723, 156);
+            this.btnRegistodeUtilizadores.Name = "btnRegistodeUtilizadores";
+            this.btnRegistodeUtilizadores.Size = new System.Drawing.Size(208, 58);
+            this.btnRegistodeUtilizadores.TabIndex = 15;
+            this.btnRegistodeUtilizadores.Text = "Registo de \r\nUtilizadores";
+            this.btnRegistodeUtilizadores.UseVisualStyleBackColor = false;
+            this.btnRegistodeUtilizadores.Click += new System.EventHandler(this.btnRegistodeUtilizadores_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnAtualizar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(492, 156);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(208, 58);
+            this.btnAtualizar.TabIndex = 14;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Coral;
+            this.btnEliminar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(264, 156);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(208, 58);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnGravar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(35, 156);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(208, 58);
+            this.btnGravar.TabIndex = 12;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = false;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 662);
+            this.Controls.Add(this.dgvContatos);
+            this.Controls.Add(this.txttelefone);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRegistodeUtilizadores);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGravar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvContatos;
+        private System.Windows.Forms.TextBox txttelefone;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistodeUtilizadores;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGravar;
+    }
+}
+
---
+++ ExemploLigacaoWF_MSSMS/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploLigacaoWF_MSSMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string connectionString = "Data Source=LEONARDO\\SQLEXPRESS;Initial Catalog=ContatosDB;Integrated Security=True";
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            string nome = txtnome.Text;
+            string email = txtemail.Text;
+            int numeroTlfn = Convert.ToInt32(txttelefone.Text);
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Contatos (ct_Id, ct_nome, ct_email, ct_telefone) VALUES (@ct_Id, @ct_nome, @ct_email, @ct_telefone)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                   
+                    command.Parameters.AddWithValue("@ct_nome", nome);
+                    command.Parameters.AddWithValue("@ct_email", email);
+                    command.Parameters.AddWithValue("@ct_telefone", numeroTlfn);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Registo efetuado com sucesso!");
+                    }
+                    else
+                    {
+                        MessageBox.Show(nome, ", erro ao efetuar registo.",
+                            (MessageBoxButtons)MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnRegistodeUtilizadores_Click(object sender, EventArgs e)
+        {     
+            string query = "SELECT * FROM Contatos";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+
+                dataAdapter.Fill(dataTable);
+
+                dgvContatos.DataSource = dataTable;
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Contatos";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+
+                dataAdapter.Fill(dataTable);
+
+                dgvContatos.DataSource = dataTable;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvContatos.SelectedRows.Count > 0)
+            {
+                int idRowSelected = Convert.ToInt32(dgvContatos.SelectedRows[0].Cells["ct_Id"].Value);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM Contatos WHERE ct_Id = @ct_Id";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ct_Id", idRowSelected);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                            MessageBox.Show("\tContato eliminado com sucesso!");
+                        else
+                            MessageBox.Show("\tNão foi possivel eliminar o contato.");
+                    }
+                }
+            }
+        }
+
+        private void dgvContatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvContatos.Rows[e.RowIndex];
+
+                txtnome.Text = row.Cells["ct_nome"].Value.ToString();
+                txtemail.Text = row.Cells["ct_nome"].Value.ToString();
+                txttelefone.Text = row.Cells["ct_nome"].Value.ToString();
+            }
+
+        }
+    }
+}
